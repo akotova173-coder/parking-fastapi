05e7c96471758fa4def697a8791d015cd59356e8
akotova173-coder <akotova173@gmail.com>
2026-09-10 22:42:16 +0300

    Update conftest.py

--- parking_fastapi/tests/conftest.py
+++ parking_fastapi/tests/conftest.py
@@ -30,7 +30,6 @@ app.dependency_overrides[get_db] = override_get_db
 
 @pytest.fixture(scope="function")
 
-
 def db_session():
     Base.metadata.create_all(bind=engine)
     session = TestingSessionLocal()
@@ -40,7 +39,6 @@ def db_session():
 
 
 @pytest.fixture(scope="function")
-
 
 def client(db_session):
     client_obj = Client(
