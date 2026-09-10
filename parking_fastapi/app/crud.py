b453c8e0ab91258663d1d726c3672024b05a34fa
akotova173-coder <akotova173@gmail.com>
2026-09-10 21:18:35 +0300

    Update crud.py

--- parking_fastapi/app/crud.py
+++ parking_fastapi/app/crud.py
@@ -9,7 +9,7 @@ def get_clients(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Client).offset(skip).limit(limit).all()
 
 def create_client(db: Session, client: schemas.ClientCreate):
-    **client.model_dump()
+    db_client = models.Client(**client.model_dump())
     db.add(db_client)
     db.commit()
     db.refresh(db_client)
